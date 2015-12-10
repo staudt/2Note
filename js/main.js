f33321d22e6c4f514c061ac588f7b0fe134c46a6
Ricardo Staudt <ricardost@gmail.com>
2015-12-10 15:40:03 -0200

    Tab key support

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 (function () {
   "use strict";
 
+    document.getElementById('content').onkeydown = function(e) {
+        if(e.keyCode==9) {
+            e.preventDefault();
+            /*
+                TODO: When it is a selection, ident with (maybe):
+                document.execCommand('styleWithCSS',true,null);
+                document.execCommand('indent',true,null);
+            */
+            var sel = window.getSelection().getRangeAt(0);
+            var tab = document.createTextNode('\t');
+            sel.deleteContents();
+            sel.insertNode(tab);
+            sel.setStartAfter(tab);
+        }
+    }
+
 })();
